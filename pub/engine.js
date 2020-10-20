a93bff6796479f967fa65b5ace78f4ea07d8dcf6
altilunium <70379302+altilunium@users.noreply.github.com>
2020-10-20 14:24:15 +0700

    ErgonomicScroll

--- pub/engine.js
+++ pub/engine.js
@@ -1,3 +1,193 @@
+//Caret.JS
+/* jshint browser: true */
+
+(function () {
+
+// We'll copy the properties below into the mirror div.
+// Note that some browsers, such as Firefox, do not concatenate properties
+// into their shorthand (e.g. padding-top, padding-bottom etc. -> padding),
+// so we have to list every single property explicitly.
+var properties = [
+  'direction',  // RTL support
+  'boxSizing',
+  'width',  // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+  'height',
+  'overflowX',
+  'overflowY',  // copy the scrollbar for IE
+
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+  'borderStyle',
+
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
+  'fontStyle',
+  'fontVariant',
+  'fontWeight',
+  'fontStretch',
+  'fontSize',
+  'fontSizeAdjust',
+  'lineHeight',
+  'fontFamily',
+
+  'textAlign',
+  'textTransform',
+  'textIndent',
+  'textDecoration',  // might not make a difference, but better be safe
+
+  'letterSpacing',
+  'wordSpacing',
+
+  'tabSize',
+  'MozTabSize'
+
+];
+
+var isBrowser = (typeof window !== 'undefined');
+var isFirefox = (isBrowser && window.mozInnerScreenX != null);
+
+function getCaretCoordinates(element, position, options) {
+  if (!isBrowser) {
+    throw new Error('textarea-caret-position#getCaretCoordinates should only be called in a browser');
+  }
+
+  var debug = options && options.debug || false;
+  if (debug) {
+    var el = document.querySelector('#input-textarea-caret-position-mirror-div');
+    if (el) el.parentNode.removeChild(el);
+  }
+
+  // The mirror div will replicate the textarea's style
+  var div = document.createElement('div');
+  div.id = 'input-textarea-caret-position-mirror-div';
+  document.body.appendChild(div);
+
+  var style = div.style;
+  var computed = window.getComputedStyle ? window.getComputedStyle(element) : element.currentStyle;  // currentStyle for IE < 9
+  var isInput = element.nodeName === 'INPUT';
+
+  // Default textarea styles
+  style.whiteSpace = 'pre-wrap';
+  if (!isInput)
+    style.wordWrap = 'break-word';  // only for textarea-s
+
+  // Position off-screen
+  style.position = 'absolute';  // required to return coordinates properly
+  if (!debug)
+    style.visibility = 'hidden';  // not 'display: none' because we want rendering
+
+  // Transfer the element's properties to the div
+  properties.forEach(function (prop) {
+    if (isInput && prop === 'lineHeight') {
+      // Special case for <input>s because text is rendered centered and line height may be != height
+      if (computed.boxSizing === "border-box") {
+        var height = parseInt(computed.height);
+        var outerHeight =
+          parseInt(computed.paddingTop) +
+          parseInt(computed.paddingBottom) +
+          parseInt(computed.borderTopWidth) +
+          parseInt(computed.borderBottomWidth);
+        var targetHeight = outerHeight + parseInt(computed.lineHeight);
+        if (height > targetHeight) {
+          style.lineHeight = height - outerHeight + "px";
+        } else if (height === targetHeight) {
+          style.lineHeight = computed.lineHeight;
+        } else {
+          style.lineHeight = 0;
+        }
+      } else {
+        style.lineHeight = computed.height;
+      }
+    } else {
+      style[prop] = computed[prop];
+    }
+  });
+
+  if (isFirefox) {
+    // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+    if (element.scrollHeight > parseInt(computed.height))
+      style.overflowY = 'scroll';
+  } else {
+    style.overflow = 'hidden';  // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
+  }
+
+  div.textContent = element.value.substring(0, position);
+  // The second special handling for input type="text" vs textarea:
+  // spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+  if (isInput)
+    div.textContent = div.textContent.replace(/\s/g, '\u00a0');
+
+  var span = document.createElement('span');
+  // Wrapping must be replicated *exactly*, including when a long word gets
+  // onto the next line, with whitespace at the end of the line before (#7).
+  // The  *only* reliable way to do that is to copy the *entire* rest of the
+  // textarea's content into the <span> created at the caret position.
+  // For inputs, just '.' would be enough, but no need to bother.
+  span.textContent = element.value.substring(position) || '.';  // || because a completely empty faux span doesn't render at all
+  div.appendChild(span);
+
+  var coordinates = {
+    top: span.offsetTop + parseInt(computed['borderTopWidth']),
+    left: span.offsetLeft + parseInt(computed['borderLeftWidth']),
+    height: parseInt(computed['lineHeight'])
+  };
+
+  if (debug) {
+    span.style.backgroundColor = '#aaa';
+  } else {
+    document.body.removeChild(div);
+  }
+
+  return coordinates;
+}
+
+if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
+  module.exports = getCaretCoordinates;
+} else if(isBrowser) {
+  window.getCaretCoordinates = getCaretCoordinates;
+}
+
+}());
+
+
+//End of Caret.JS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Get Base URL
 //Example : if URL is http://localhost/r/Main/Internet 
 //          then, base URL is http://localhost/r/Main/
@@ -23,6 +213,17 @@ function getBaseURL() {
 	}
 	return thisPageURL;
 }
+
+function getStat(){
+	var a = document.getElementById("text");
+	console.log("clientHeight" + a.clientHeight);
+	console.log("cols" + a.cols);
+	console.log("rows" + a.rows);
+	console.log("scrollHeight" + a.scrollHeight);
+	console.log("selectionEnd" + a.selectionEnd);
+
+}
+
 
 //Convert string to title case
 //Example: if input is ababa
@@ -119,12 +320,12 @@ var nodeSourceCode = "";
 //Asynchronous Save Changes
 //Called once every 3 minutes
 function saveChanges(){
-	console.log("Past Node Source Code :" + nodeSourceCode);
+	//console.log("Past Node Source Code :" + nodeSourceCode);
 	var n = document.querySelector("form[name='nodeeditor'] input[name='n']").value
 	var basetime = document.querySelector("form[name='nodeeditor'] input[name='basetime']").value
 	var textContent = document.querySelector("#text").value
 	if (textContent == nodeSourceCode) {
-		console.log("No changes, abort autosave..");
+		//console.log("No changes, abort autosave..");
 		return 0;
 	}
 
@@ -156,7 +357,7 @@ function saveChanges(){
 
 			}, 1000);
 			e.text().then(function(r){
-				console.log(r)
+				//console.log(r)
 				var retrievedHTML = r 
 				var newDateRegex = /name='basetime' value='(.*)'/g;
 				var match = newDateRegex.exec(r)
@@ -411,6 +612,21 @@ var textarea = window.document.querySelector("textarea");
 if(textarea){	
 	textarea.onkeyup = function(e){
 
+		var caretCoord = getCaretCoordinates(textarea, textarea.selectionEnd, { debug: true });
+		//console.log('Caret top = %s', caretCoord.top);
+		//console.log('Window Scroll Height = %s', window.scrollY)
+		var ergoNumber = caretCoord.top - window.scrollY;
+		//console.log('Ergo Number = %s', ergoNumber);
+
+		if (ergoNumber >= 250) {
+			//console.log("Bad ergonomy! %s",ergoNumber);
+			window.scrollBy(0,125);	
+		}
+		
+
+
+
+
 		if (textarea.clientHeight < textarea.scrollHeight){
 			textarea.style.height = textarea.scrollHeight +"px";
 		}
@@ -479,10 +695,10 @@ $(function(){
       $('.demo-noninputable').pastableNonInputable();
       $('.demo-textarea').on('focus', function(){
         var isFocused = $(this).hasClass('pastable-focus');
-        console && console.log('[textarea] focus event fired! ' + (isFocused ? 'fake onfocus' : 'real onfocus'));
+        //console && console.log('[textarea] focus event fired! ' + (isFocused ? 'fake onfocus' : 'real onfocus'));
       }).pastableTextarea().on('blur', function(){
         var isFocused = $(this).hasClass('pastable-focus');
-        console && console.log('[textarea] blur event fired! ' + (isFocused ? 'fake onblur' : 'real onblur'));
+        //console && console.log('[textarea] blur event fired! ' + (isFocused ? 'fake onblur' : 'real onblur'));
       });
       $('.demo-contenteditable').pastableContenteditable();
       $('.demo').on('pasteImage', function(ev, data){
